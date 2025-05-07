af3b2386e0b5fbc719e2b39e4209ace28d0451d0
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-05-07 14:09:53 +0530

    exits and valid

--- backend/controllerss/authcontroll.js
+++ backend/controllerss/authcontroll.js
@@ -1,9 +1,20 @@
 const db=require('../db')
 const register= async(req,res)=>{
-   res.status(200).json({
-    msg:"conected"
-   })
-   console.log(req.body)
+  try{
+    const {username,password,contact,email}=req.body
+    if(!username||!password ||!email){
+        return res.status(400).json({
+            msg:"usrnmae email passwrod must require"
+        })
+    }
+ const[existing]=await db.query("select * from users where username=? or email =?",[username,email])
+ console.log(existing)
+
+  }catch(err){
+    res.status(500)
+    throw new Error(err.message)
+  }
+
 }
 
 
