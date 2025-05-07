3aae1683d6117430992e07dd36633ae8141dc7aa
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-05-07 17:02:37 +0530

    creeate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter,Route,Routes}from'react-router-dom'
+const App = () => {
+  return (
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+
+  </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
