12dc47d7d23ebdafa8d43191eb912e0728d3ec88
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-05-07 14:19:31 +0530

    1st user

--- backend/controllerss/authcontroll.js
+++ backend/controllerss/authcontroll.js
@@ -1,14 +1,26 @@
 const db=require('../db')
+const enct=require('bcryptjs')
 const register= async(req,res)=>{
   try{
     const {username,password,contact,email}=req.body
+    const profile_image=null
     if(!username||!password ||!email){
         return res.status(400).json({
             msg:"usrnmae email passwrod must require"
         })
     }
- const[existing]=await db.query("select * from users where username=? or email =?",[username,email])
- console.log(existing)
+ 
+    const[existing]=await db.query("select * from users where username=? or email =?",[username,email])
+    if(existing.length>0){
+        return res.status(400).json({msg:"already exists"})
+    }
+
+const hashpass= await enct.hash(password,10)
+const sql="insert into users (username,email,contact,password,profile_image) values(?,?,?,?,?)"
+await db.query(sql,[username,email,contact||null,hashpass,profile_image])
+res.status(201).json({
+    msg:"creatd successfullly "
+})
 
   }catch(err){
     res.status(500)
